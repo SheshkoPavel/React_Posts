ce66a6789f9e2ee4cb316c5cb6a0028c5a02a8aa
Sheshko Pavel <derzurb@mail.ru>
2022-01-23 16:35:58 +0300

    Added ClassCounter.jsx component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
-import React, {useState} from 'react';
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
 
-  const [likes, setLikes] = useState(5)
-  const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ')
-
-  function increment () {
-    setLikes(likes + 1)
-
-  }
-
-  function decrement () {
-    setLikes(likes - 1)
-  }
-
-
+  // const [likes, setLikes] = useState(5)
+  // const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ')
 
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
+        Это функциональные компоненты
+      <Counter/>
+      <Counter/>
 
-      <input
-          type="text"
-          value={value}
-          onChange={event => setValue(event.target.value)}
-      />
+       <br/>
+        Это классовый компонент
+      <ClassCounter/>
 
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
